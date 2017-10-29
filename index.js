75e4d21110835b152c01809c8dc347e4f1b0c887
Pavel Tarno <paveltarno@gmail.com>
2017-10-29 22:51:35 +0200

    wow

--- index.js
+++
@@ -1,6 +0,0 @@
-const url =
-  "http://us.asos.com/asos-petite/asos-petite-smock-dress-in-ponte-stripe/prd/7519285/?clr=blue&SearchQuery=981721&SearchRedirect=true";
-
-function extractProductStatus(product) {}
-
-console.log("");
