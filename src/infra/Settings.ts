88eda4e216eda8f57588c184e5bfc596e26a8dd5
Pavel Tarno <paveltarno@gmail.com>
2018-01-27 18:21:51 +0200

    wip

--- src/infra/Settings.ts
+++ src/infra/Settings.ts
@@ -1,4 +1,6 @@
-import { reduce, set } from 'lodash';
+import { reduce, set, indexOf, slice } from 'lodash';
+
+const NAMESPACE_PREFIX = 'asos_tracker';
 
 interface Settings {
   scraper: {
@@ -10,6 +12,16 @@ interface Settings {
 // Load settings
 export const settings = reduce(
   process.env,
-  (acc, v, k) => set(acc, k, v),
+  (acc, v, k: string) => {
+    const index = indexOf(k, '.');
+
+    if (index > 0) {
+      const [prefix, key] = [k.slice(0, index), k.slice(index + 1)];
+      if (prefix === NAMESPACE_PREFIX) {
+        set(acc, k, v);
+      }
+    }
+    return acc;
+  },
   {}
 ) as Settings;
