efffb57d797f052963e00c4822a096d889c5237b
moriaam <moria.am@gmail.com>
2018-01-28 10:16:42 +0200

    display unavailable sizes in checkboxes

--- chrome_extension/contentscript.js
+++ chrome_extension/contentscript.js
@@ -3,12 +3,9 @@ var port = chrome.runtime.connect()
 window.addEventListener(
   'message',
   function(event) {
-    // We only accept messages from ourselves
     if (event.source != window) return
 
     if (event.data.type && event.data.type == 'FROM_PAGE') {
-      console.log('Content script received: ' + event.data.sizes)
-
       chrome.runtime.sendMessage({ sizes: event.data.sizes }, function(response) {
       })
     }
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    let sizesList = document.getElementById('sizes')
+    let container = document.getElementById('container')
 
     for (var index = 0; index < request.sizes.length; index++) {
-      let item = document.createElement('li')
-      item.innerText = request.sizes[index]
-      sizesList.appendChild(item)
+      let size = request.sizes[index]
+      
+      let div = document.createElement('div')
+      
+      const id = `size-checkbox-${index}`
+      
+      let checkbox = document.createElement('input')
+      checkbox.type = "checkbox"
+      checkbox.name = "name"
+      checkbox.value = size
+      checkbox.id = id
+      
+      let label = document.createElement('label')
+      label.htmlFor = id
+      label.appendChild(document.createTextNode(size))
+      
+      div.appendChild(checkbox)
+      div.appendChild(label)
+      container.appendChild(div)
     }
   })
 
@@ -16,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (var index = 0; index < sizeSelector.children.length; index++) {
       var element = sizeSelector.children[index]
-      if (element.text !== 'Please select' && element.text.indexOf('Not available') === -1)
+      if (element.text !== 'Please select' && element.text.indexOf('Not available') !== -1)
         available.push(element.text)
     }
 
